abb3794acd52ba994d782a8f2bdddaea07e9dfcf
Yaga Gauganek <agataeblumkowska@gmail.com>
2023-08-03 11:42:32 +0100

    components and pages add

---
+++ client/src/Components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Footer.css"
+
+export default function Footer() {
+  return (
+<footer>
+    <h6>kjg</h6>
+</footer>
+  )
+}
---
+++ client/src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.ss'
+
+export default function Home() {
+  return (
+    <main>
+        <h3>lgkc</h3>
+    </main>
+
+  )
+}
